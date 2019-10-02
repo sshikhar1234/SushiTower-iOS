681bede6bcad93eed1268361c67e48e86fe431c1
sshikhar1234 <sshikharshah@gmail.com>
2019-10-02 10:02:05 -0400

    Remove sushi on cat punch and bring whole tower down

--- SushiTower/GameScene.swift
+++ SushiTower/GameScene.swift
@@ -55,6 +55,23 @@ class GameScene: SKScene {
         sushiTower.append(currentSushi)
     }
     
+    //Cat hitting the base but the piece on top of base should be removed and the whole towert should come down
+    func removeSuShi(){
+        //Get the sushi on top of base
+        var sushiToRemove = self.sushiTower[0]
+
+        //Bring the whole arrray down
+        for sushi in self.sushiTower{
+            let dropAction  = SKAction.move(to: CGPoint(x: sushi.position.x, y: sushi.position.y-100), duration: 0.1)
+            sushi.run(dropAction)
+        }
+        //Remove that sushi from screen
+        sushiToRemove.removeFromParent()
+        
+        //Remove that sushi from Array
+        self.sushiTower.remove(at: 0)
+        
+    }
     
     override func update(_ currentTime: TimeInterval) {
     }
@@ -111,6 +128,7 @@ class GameScene: SKScene {
         }
         self.cat.run(punchAnimation, completion: {
             //This gets executed when the animation is done running for 0.2 seconds
+            self.removeSuShi()
         })
         
         print(mousePosition)
